0bf76ca0828848a7707579768ea086b42b9885eb
OziArantha <muhammad.sairozi.1903126@students.um.ac.id>
2021-11-09 19:11:37 +0700

    representasi data

---
+++ DailyTask/data/Dailytask.java
@@ -0,0 +1,26 @@
+package data;
+
+public class Dailytask
+{
+    private String task;
+    
+    public Dailytask()
+    {
+        
+    }
+    
+    public Dailytask(String task)
+    {
+        this.task = task;
+    }
+    
+    public String getTask()
+    {
+        return task;
+    }
+    
+    public void setTask(String task)
+    {
+        this.task = task;
+    }
+}
