a5b4e5a22f2daa1ef45eced65ed3783c73d65f78
OziArantha <muhammad.sairozi.1903126@students.um.ac.id>
2021-11-12 21:23:57 +0700

    nambahin util dan view

--- DailyTask/AplikasiDailyTask.java
+++ DailyTask/AplikasiDailyTask.java
@@ -1,8 +1,17 @@
+import repository.DailyTaskRepository;
+import repository.DailyTaskRepositoryImpl;
+import service.DailyTaskService;
+import service.DailyTaskServiceImpl;
+import view.DailyTaskView;
 
 public class AplikasiDailyTask
 {
     public static void main(String[] args)
     {
-        //percobaan komen gan
+        DailyTaskRepository dailyTaskRepository = new DailyTaskRepositoryImpl();
+        DailyTaskService dailyTaskService = new DailyTaskServiceImpl(dailyTaskRepository);
+        DailyTaskView dailyTaskView = new DailyTaskView(dailyTaskService);
+
+        dailyTaskView.showDailyTask();
     }
 }
